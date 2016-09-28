35f8fc9a2d1d2b3f47ab8595fbda10375d643335
0stvind <zakhar.kor@gmail.com>
2016-09-29 00:44:12 +0400

    Add files via upload

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,15 @@
+# -*- coding: utf-8 -*-
 import json
 
 
 def load_data(filepath):
-    pass
-
+    with open(filepath, 'r') as file_handler:
+      pretty_print_json(json.load(file_handler))
+      
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent = 4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    pass
+    load_data('alc.json')
