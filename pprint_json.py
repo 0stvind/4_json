458cae621c2f14ffcc6131e60467ae379b4e920b
0stvind <zakhar.kor@gmail.com>
2016-10-07 23:26:59 +0400

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -4,12 +4,14 @@ import json
 
 def load_data(filepath):
     with open(filepath, 'r') as file_handler:
-      pretty_print_json(json.load(file_handler))
-      
+        return json.load(file_handler)
+
 
 def pretty_print_json(data):
-    print(json.dumps(data, indent = 4, ensure_ascii=False))
+    print(json.dumps(data, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    load_data('alc.json')
+    print('Введите ПОЛНЫЙ путь к файлу')
+    filepath = input()
+    pretty_print_json(load_data(filepath))
